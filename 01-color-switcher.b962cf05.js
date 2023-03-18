930d38253385c58aeabe98c7a3ae85be67288cda
DmytroSkoryk <114485103+DmytroSkoryk@users.noreply.github.com>
2023-03-18 18:17:19 +0000

    Deploying to gh-pages from @ DmytroSkoryk/goit-js-hw-09@d22d754501ba054d3190db8925169093292b4691 🚀

---
+++ 01-color-switcher.b962cf05.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};t.startBtn.addEventListener("click",(function(){timerId=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.startBtn.disabled=!0,t.stopBtn.disabled=!1})),t.stopBtn.addEventListener("click",(function(){clearInterval(timerId),t.startBtn.disabled=!1,t.stopBtn.disabled=!0})),t.stopBtn.disabled=!0;
+//# sourceMappingURL=01-color-switcher.b962cf05.js.map
